0ea9d416d856a7f22baf4721809077c91061a34c
Melrizzuto <melrizzuto4@gmail.com>
2025-04-04 14:30:24 +0200

    milestone2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,21 @@
+import { useState } from "react";
+
 function App() {
+  const [addedProduct, setAddedProduct] = useState([]);
+
+  const addToCart = (product) => {
+    const productIsAlreadyAdd = addedProduct.some(p => p.name === product.name);
+    if (productIsAlreadyAdd) {
+      return;
+    }
+    setAddedProduct(curr => [
+      ...curr,
+      {
+        ...product,
+        quantity: 1
+      }
+    ]);
+  };
 
   const products = [
     { name: 'Mela', price: 0.5 },
@@ -9,25 +26,34 @@ function App() {
 
   return (
     <>
-      <h1>Prodotti tra cui scegliere</h1>
+      <h1>Prodotti disponibili</h1>
       <ul>
-        {
-          products.map((product, index) => (
-
-            <li key={index}>
-              <p>
-                {product.name} ({product.price.toFixed(2)}€)
-              </p>
-            </li>
-
-          ))
-        }
-
+        {products.map((product, index) => (
+          <li key={index}>
+            <p>
+              {product.name} - {product.price.toFixed(2)}€
+            </p>
+            <button onClick={() => addToCart(product)}>
+              Aggiungi al carrello
+            </button>
+          </li>
+        ))}
       </ul>
+
+      {addedProduct.length > 0 && (
+        <>
+          <h2>Carrello</h2>
+          <ul>
+            {addedProduct.map((item, index) => (
+              <li key={index}>
+                {item.name} - {item.price.toFixed(2)}€ x {item.quantity}
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </>
-
-
-  )
+  );
 }
 
-export default App
+export default App;
